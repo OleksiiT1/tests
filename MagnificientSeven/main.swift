c33568df0e59444b88ce3621c1bfe02392a20614
Student-2 <student-2@macbook-pro-it-2.local>
2018-11-03 14:58:34 +0200

    Initial Commit

---
+++ MagnificientSeven/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MagnificientSeven
+//
+//  Created by Student-2 on 11/3/18.
+//  Copyright © 2018 Student-2. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
